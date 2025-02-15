6286b1cdf45ff3468be6b6c6d3884d80686b7600
jgaillard617 <jgaillard717@gmail.com>
2025-02-15 18:53:51 +0000

    Sample form

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var bdpahistoryRouter = require('./routes/bdpahistory');
+var formentryRouter = require('./routes/formentry');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/bdpahistory',bdpahistoryRouter);
+app.use('/formentry', formentryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
